48f78ce9662927d8469ebdc7b43ad412168dda66
atuan_h <atuan_h@hotmail.com>
2015-07-28 01:12:59 +0700

    Change to Fluent mode

---
+++ MyERP.DataAccess/FluentModel/SessionVariable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyERP.DataAccess
+{
+    public class SessionVariable
+    {
+        public Organization Organization { get; set; }
+        public Client Client { get; set; }
+    }
+}
